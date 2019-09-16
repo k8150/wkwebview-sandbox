402bab78ca388544c902adc2aa39104061871d9f
k.haigo <kojihaigo@gmail.com>
2019-09-16 21:35:34 +0900

    initial commit

---
+++ wkwebview-sandbox/source/ImageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ImageViewController.swift
+//  wkwebview-sandbox
+//
+//  Created by haigo koji on 2019/09/16.
+//  Copyright © 2019 haigo koji. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
